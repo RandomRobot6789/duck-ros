2d33c1778a700abae334b292f303b21e7fb7cccc
RandomRobot6789 <jenkinsandrewp@gmail.com>
2026-09-18 16:53:24 -0600

    publish-subscribe nodes

---
+++ launchers/my-subscriber.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /environment.sh
+
+dt-launchfile.init
+
+rosrun my_package my_subscriber_node.py
+
+dt-launchfile-join
---
+++ packages/my_package/src/my_subscriber_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from std_msgs.msg import String
+
+class MySubscriberNode(DTROS):
+    def __init__(self, node_name):
+        super(MySubscriberNode, self).__init__(node_name = node_name, node_type = NodeType.GENERIC)
+        self.sub = rospy.Subscriber('chatter', String, self.callback)
+
+    def callback(self, data):
+        rospy.loginfo("I heard 's%'", data.data)
+    
+if __name__ = '__main__':
+    node = MySubscriberNode(node_name = 'my_subscriber_node')
+
+    rospy.spin()
